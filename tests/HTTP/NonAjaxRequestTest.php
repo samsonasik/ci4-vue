272ac27ce0e6e059f2898962cfd39a6335fbb12f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-07-02 19:39:43 +0700

    100% test coverage

---
+++ tests/HTTP/NonAjaxRequestTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\HTTP;
+
+use CodeIgniter\Test\FeatureTestCase;
+
+class NonAjaxRequestTest extends FeatureTestCase
+{
+    public function testRenderLayout()
+    {
+        $routes = [
+            [ 'get', 'home', 'Home::index' ]
+        ];
+
+        ob_start();
+        $this->withRoutes($routes)
+            ->get('home');
+        $layout = ob_get_clean();
+
+        $this->assertRegExp('/<html>/', $layout);
+    }
+}
