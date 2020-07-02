0e649e76c38c09a3dc41a6cf95efbdf9db94e00c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-07-02 19:13:40 +0700

    add test before on is api

--- tests/unit/Filters/XMLHttpRequestTest.php
+++ tests/unit/Filters/XMLHttpRequestTest.php
@@ -3,12 +3,29 @@
 namespace Tests\unit\Filters;
 
 use App\Filters\XMLHttpRequest;
+use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use PHPUnit\Framework\TestCase;
 
 class XMLHttpRequestTest extends TestCase
 {
+	private $filter;
+
+	protected function setUp() : void
+	{
+		$this->filter = new XMLHttpRequest();
+	}
+
+	public function testBeforeOnIsAPI()
+	{
+		$request = $this->prophesize(IncomingRequest::class);
+		$request->detectPath()->willReturn('api/portfolio');
+		$request->hasHeader('X-Requested-With')->willReturn(false);
+
+		$this->assertNull($this->filter->before($request->reveal()));
+	}
+
 	public function testAfter()
 	{
 		$filter = new XMLHttpRequest();
