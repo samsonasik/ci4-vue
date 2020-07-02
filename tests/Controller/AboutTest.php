5d0ae864f9d48cc01c70ea019a80c5f1b2f9a768
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-07-02 19:22:41 +0700

    add About page test

---
+++ tests/Controller/AboutTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Controller;
+
+use App\Controllers\About;
+use CodeIgniter\Test\CIUnitTestCase;
+use CodeIgniter\Test\ControllerTester;
+
+class AboutTest extends CIUnitTestCase
+{
+	use ControllerTester;
+
+	public function testIndex()
+	{
+		$result = $this->controller(About::class)
+						->execute('index');
+
+		$this->assertTrue($result->isOK());
+		$this->assertTrue($result->see('I\'m a web developer. My name is {{ this.$parent.name }}'));
+	}
+}
