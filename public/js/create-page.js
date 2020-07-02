79375c77e8fb0e20e409cfb7ebfdb3aadfb062f0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-07-02 18:49:57 +0700

    remove unnecessary return keyword

--- public/js/create-page.js
+++ public/js/create-page.js
@@ -1,29 +1,27 @@
-let createPage = (name, data = {}, methods = {}, updated = () => {}) => {
-    return Vue.component('page-' + name, {
-        data: () => Object.assign({ content: 'Loading...'}, data),
-        methods: methods,
-        mounted () {
-            (new Promise( (resolve) => {
-                fetch(
-                    this.$route.path,
-                    {
-                        method: 'GET',
-                        headers: {
-                            'X-Requested-With': 'XMLHttpRequest',
-                        }
+let createPage = (name, data = {}, methods = {}, updated = () => {}) => Vue.component('page-' + name, {
+    data: () => Object.assign({ content: 'Loading...'}, data),
+    methods: methods,
+    mounted () {
+        (new Promise( (resolve) => {
+            fetch(
+                this.$route.path,
+                {
+                    method: 'GET',
+                    headers: {
+                        'X-Requested-With': 'XMLHttpRequest',
                     }
-                ).then(response =>  resolve(response.text()));
-            })).then(result => this.content = result);
-        },
-        render : function (c) {
-            if (this.content == '') {
-                return;
-            }
+                }
+            ).then(response =>  resolve(response.text()));
+        })).then(result => this.content = result);
+    },
+    render : function (c) {
+        if (this.content == '') {
+            return;
+        }
 
-            return c(Vue.compile('<div>' + this.content + '</div>'));
-        },
-        updated: updated
-    });
-}
+        return c(Vue.compile('<div>' + this.content + '</div>'));
+    },
+    updated: updated
+});
 
 export default createPage;
