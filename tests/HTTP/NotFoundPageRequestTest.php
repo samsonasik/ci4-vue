316b127954d2176fb89fb862cfa057d98f01d37f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-07-03 08:29:01 +0700

    test for not found handling

---
+++ tests/HTTP/NotFoundPageRequestTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\HTTP;
+
+use CodeIgniter\Exceptions\PageNotFoundException;
+use CodeIgniter\Test\FeatureTestCase;
+
+class NotFoundPageRequestTest extends FeatureTestCase
+{
+    public function testLoad404Template()
+    {
+        $routes = [
+            [ 'get', '404', '404::index' ]
+        ];
+
+        $this->expectException(PageNotFoundException::class);
+        $this->withRoutes($routes)
+            ->get('404');
+    }
+}
