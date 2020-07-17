9283caf1cb5af21d6999f3e8f5893c1e72de6b17
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-07-17 18:03:15 +0700

    apply mutation testing

--- tests/Controller/Api/PortfolioTest.php
+++ tests/Controller/Api/PortfolioTest.php
@@ -19,19 +19,84 @@ class PortfolioTest extends CIUnitTestCase
 		$this->assertTrue($result->see('website'));
 	}
 
-	public function testIndexSearchPortfolioFound()
+	public function provideTitle()
+	{
+		return [
+			['Website B'],
+			['site B'],
+		];
+	}
+
+	/**
+	 * @dataProvider provideTitle
+	 */
+	public function testIndexSearchPortfolioFoundByTitle($title)
 	{
 		$request = service('request');
 		$request->setMethod('get');
 		$request->setGlobal('get', [
-			'keyword' => 'website',
+			'keyword' => $title,
 		]);
 
 		$result = $this->withRequest($request)
 					   ->controller(Portfolio::class)
 					   ->execute('index');
 
-		$this->assertTrue($result->see('website'));
+		$this->assertTrue($result->see($title));
+	}
+
+	public function provideLink()
+	{
+		return [
+			['https://www.website-b.com'],
+			['www.website-b.com'],
+		];
+	}
+
+	/**
+	 * @dataProvider provideLink
+	 */
+	public function testIndexSearchPortfolioFoundByLink($link)
+	{
+		$request = service('request');
+		$request->setMethod('get');
+		$request->setGlobal('get', [
+			'keyword' => $link,
+		]);
+
+		$result = $this->withRequest($request)
+					   ->controller(Portfolio::class)
+					   ->execute('index');
+
+		$this->assertTrue($result->see($link));
+	}
+
+	public function provideEmptyKeyword()
+	{
+		return [
+			[null],
+			[''],
+		];
+	}
+
+	/**
+	 * @dataProvider provideEmptyKeyword
+	 */
+	public function testIndexSearchPortfolioEmptyKeywordShowAll($keyword)
+	{
+		$request = service('request');
+		$request->setMethod('get');
+		$request->setGlobal('get', [
+			'keyword' => $keyword,
+		]);
+
+		$result = $this->withRequest($request)
+					   ->controller(Portfolio::class)
+					   ->execute('index');
+
+		$this->assertTrue($result->see('website-a'));
+		$this->assertTrue($result->see('website-b'));
+		$this->assertTrue($result->see('website-c'));
 	}
 
 	public function testIndexSearchPortfolioNotFound()
@@ -39,13 +104,13 @@ class PortfolioTest extends CIUnitTestCase
 		$request = service('request');
 		$request->setMethod('get');
 		$request->setGlobal('get', [
-			'keyword' => 'website-abcdef',
+			'keyword' => 'website-abcdef'
 		]);
 
 		$result = $this->withRequest($request)
 					   ->controller(Portfolio::class)
 					   ->execute('index');
 
-		$this->assertFalse($result->see('website-abcdef'));
+		$this->assertFalse($result->see('website'));
 	}
 }
