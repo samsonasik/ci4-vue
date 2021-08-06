77415f2b65ee9828b62951b2d964e7e8f40eabf3
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-07 03:19:24 +0700

    apply rector

---
+++ rector.php
@@ -0,0 +1,20 @@
+<?php
+
+use Rector\Core\Configuration\Option;
+use Rector\Core\ValueObject\PhpVersion;
+use Rector\Set\ValueObject\SetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(SetList::NAMING);
+    $containerConfigurator->import(SetList::PHP_70);
+    $containerConfigurator->import(SetList::PHP_71);
+    $containerConfigurator->import(SetList::PHP_72);
+    $containerConfigurator->import(SetList::PHP_73);
+
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [__DIR__ . '/app/Controllers', __DIR__ . '/app/Filters', __DIR__ . '/tests']);
+
+    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
+    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_73);
+};
