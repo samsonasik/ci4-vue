1eb77be1df717137db6790834f9e92e504da5c1c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-02-10 06:42:39 +0700

    update rector and support composer 2.2

--- rector.php
+++ rector.php
@@ -2,15 +2,13 @@
 
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
+use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(SetList::NAMING);
-    $containerConfigurator->import(SetList::PHP_70);
-    $containerConfigurator->import(SetList::PHP_71);
-    $containerConfigurator->import(SetList::PHP_72);
-    $containerConfigurator->import(SetList::PHP_73);
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_73);
 
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [__DIR__ . '/app/Controllers', __DIR__ . '/app/Filters', __DIR__ . '/tests']);
