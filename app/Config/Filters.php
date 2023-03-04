564abf46b93122de1acd6581af7b11f0c8645ab0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-03-04 17:24:37 +0700

    Fix cs

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -22,7 +22,7 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
-        'layoutUsage' => LayoutUsage::class,
+        'layoutUsage'   => LayoutUsage::class,
     ];
 
     /**
