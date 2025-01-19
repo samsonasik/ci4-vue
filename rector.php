98dc4355d757b8a6db0bffc50a3d3c977d946a1a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-01-20 03:33:20 +0700

    upgrade rector

--- rector.php
+++ rector.php
@@ -1,20 +1,14 @@
 <?php
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([
-        SetList::NAMING,
-        LevelSetList::UP_TO_PHP_74,
-    ]);
-
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/app/Controllers',
         __DIR__ . '/app/Filters',
         __DIR__ . '/tests',
-        __DIR__ . '/rector.php',
-    ]);
-    $rectorConfig->importNames();
-};
+    ])
+    ->withRootFiles()
+    ->withPreparedSets(naming: true)
+    ->withPhpSets(php82: true)
+    ->withImportNames();
